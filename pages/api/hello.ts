d7d61a3648e4bbbe26dd7130f536ab4370b5630e
werapol <wesakaber@gmail.com>
2025-02-05 01:05:20 +0700

    fig:bug api deploy vercel

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -22,6 +22,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.status(200).end();
+    return;
+  }
   if (req.method === "GET") {
     try {
       const result = await connectionPool.query("select * from pet");
